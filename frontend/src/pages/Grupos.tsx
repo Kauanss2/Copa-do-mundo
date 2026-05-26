88f9cafeb094f5b345b241f0c186c588619ab691
Kauan <kauanvitor.1921@gmail.com>
2026-05-25 22:57:22 -0300

    fix: prisma config production

--- frontend/src/pages/Grupos.tsx
+++ frontend/src/pages/Grupos.tsx
@@ -1,6 +1,5 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
-import { useAuth } from '../contexts/AuthContext'
 import api from '../services/api'
 import GrupoDetalhe from './GrupoDetalhe'
 
