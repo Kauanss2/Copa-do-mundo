fc9bf71cc538e58ca357a9245c55c5e54feb4e7f
Kauan <kauanvitor.1921@gmail.com>
2026-05-25 22:36:18 -0300

    fix: hardcode db connection

--- bolao-copa/backend/src/libs/prisma.ts
+++ bolao-copa/backend/src/libs/prisma.ts
@@ -3,6 +3,7 @@ import { createPool } from 'mysql2'
 
 const pool = createPool(process.env.DATABASE_URL!)
 
+console.log('Conectando ao banco de dados...')
 const prisma = new PrismaClient({ adapter: pool as any })
 
-export { prisma }
+export { prisma } 
