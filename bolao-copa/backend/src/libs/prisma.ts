2945a8642f89d95a855dcdd048085297110f916e
Kauan <kauanvitor.1921@gmail.com>
2026-05-25 22:28:54 -0300

    fix: hardcode db connection

--- bolao-copa/backend/src/libs/prisma.ts
+++ bolao-copa/backend/src/libs/prisma.ts
@@ -1,20 +1,8 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb"
 import { PrismaClient } from '../generated/prisma/client.js'
+import { createPool } from 'mysql2'
 
-const adapter = new PrismaMariaDb({
-  host:            process.env.DATABASE_HOST,
-  user:            process.env.DATABASE_USER,
-  password:        process.env.DATABASE_PASSWORD,
-  database:        process.env.DATABASE_NAME,
-  port:            Number(process.env.DATABASE_PORT),
-  connectionLimit: 5,
-})
+const pool = createPool(process.env.DATABASE_URL!)
 
-console.log('DB HOST:', process.env.DATABASE_HOST)
-console.log('DB PORT:', process.env.DATABASE_PORT)
-console.log('DB USER:', process.env.DATABASE_USER)
-console.log('DB NAME:', process.env.DATABASE_NAME)
-
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter: pool as any })
 
 export { prisma }
