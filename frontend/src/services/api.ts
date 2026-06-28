a1d9f456f7ee286d39cfca187ab06c38af0843e2
Kauan <kauanvitor.1921@gmail.com>
2026-06-28 14:51:28 -0300

    Fandagos

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || 'http://copa-do-mundo-production.up.railway.app:3333',
+baseURL: import.meta.env.VITE_API_URL || 'http://copa-do-mundo-production.up.railway.app/',
 })
 
 // Injeta o token em toda requisição automaticamente
