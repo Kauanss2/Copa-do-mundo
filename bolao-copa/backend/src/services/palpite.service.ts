c6c85afa5c457204cdaddc13afbaf846c5b73e60
Kauan <kauanvitor.1921@gmail.com>
2026-06-29 17:30:24 -0300

    Fandagosss

--- bolao-copa/backend/src/services/palpite.service.ts
+++ bolao-copa/backend/src/services/palpite.service.ts
@@ -23,7 +23,7 @@ function validarJogo(jogo: {
 
     const diffHoras = (jogo.inicioEm.getTime() - Date.now()) / (1000 * 60)
 
-    if (diffHoras <= 15) {
+    if (diffHoras >= 15) {
         throw new Error('FORA_DO_PRAZO')
     }
 
