47de4cacabb8061696e567dd2c30b6f730243532
Kauan Vitor <kauanvitor.1921@gmail.com>
2026-07-04 10:06:17 -0300

    Change time difference calculation to minutes

--- bolao-copa/backend/src/services/palpite.service.ts
+++ bolao-copa/backend/src/services/palpite.service.ts
@@ -20,7 +20,7 @@ function validarJogo(jogo: {
     if (!jogo.timeCasaId || !jogo.timeVisitanteId) {
         throw new Error('TIMES_NAO_DEFINIDOS')
     }
-const diffHoras = (jogo.inicioEm.getTime() - Date.now()) / (1000 * 60 * 60)
+const diffHoras = (jogo.inicioEm.getTime() - Date.now()) / (1000 * 60)
 
 if (diffHoras <= 15) {
     throw new Error('FORA_DO_PRAZO')
