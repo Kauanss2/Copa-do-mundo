3ad968d3bbbad33eda8d0d5623dfaf01ef870569
Kauan Vitor <kauanvitor.1921@gmail.com>
2026-07-04 13:46:50 -0300

    Change time threshold for game start check

--- frontend/src/pages/GrupoDetalhe.tsx
+++ frontend/src/pages/GrupoDetalhe.tsx
@@ -170,7 +170,7 @@ function podeApostar(jogo: any) {
   const inicioJogo = new Date(jogo.inicioEm).getTime()
 
 
- const faltamMaisDeDuasHoras = inicioJogo - agora > 15 * 60 * 1000
+ const faltamMaisDeDuasHoras = inicioJogo - agora > 7 * 60 * 1000
 
 if (!faltamMaisDeDuasHoras) {
   return false
